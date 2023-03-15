6d5a78690587a92b943a4bcde60217502d6a6f57
João Vitor <jv86679@gmail.com>
2023-03-15 13:43:44 -0300

    questões adicionadas

--- src/index.js
+++ src/index.js
@@ -34,6 +34,74 @@ const questions = [
     options: ['Pedro Álvares Cabral', 'Cristóvão Colombo', 'Bartolomeu Dias', 'Vasco da Gama'],
     answer: 'Pedro Álvares Cabral',
   },
+  {
+    text:
+      'O estudo da citologia (ou biologia celular) possibilitou a identificação de várias estruturas celulares e suas funções. Com base nesse estudo, explique a importância dos ribossomos para a síntese proteica.',
+    options: [
+      'Os ribossomos são responsáveis pela quebra de proteínas em aminoácidos',
+      'Os ribossomos são responsáveis pela síntese de ácidos nucleicos',
+      'Os ribossomos são responsáveis pela síntese de lipídios',
+      'Os ribossomos são responsáveis pela síntese de proteínas',
+    ],
+    answer: 'Os ribossomos são responsáveis pela síntese de proteínas',
+  },
+  {
+    text:
+      'Os fungos são importantes decompositores e são utilizados para produção de alimentos e medicamentos. Com base nesse estudo, explique como os fungos se reproduzem.',
+    options: [
+      'Os fungos se reproduzem assexuadamente por meio da divisão celular',
+      'Os fungos se reproduzem assexuadamente por meio de esporos',
+      'Os fungos se reproduzem sexuadamente por meio da fertilização',
+      'Os fungos se reproduzem sexuadamente por meio da clonagem',
+    ],
+    answer: 'Os fungos se reproduzem sexuadamente por meio da fertilização',
+  },
+  {
+    text:
+      'A tabela periódica é uma ferramenta fundamental para o estudo da química, que organiza os elementos químicos de acordo com suas propriedades. Com base nesse estudo, explique como é possível determinar o número de elétrons de um átomo a partir da sua posição na tabela periódica.',
+    options: [
+      'O número de elétrons de um átomo é igual ao seu número atômico',
+      'O número de elétrons de um átomo é igual ao seu número de massa',
+      'O número de elétrons de um átomo é igual ao número de prótons',
+      'O número de elétrons de um átomo é igual à sua posição na tabela periódica',
+    ],
+    answer: 'O número de elétrons de um átomo é igual ao seu número atômico',
+  },
+  {
+    text:
+      'A Geometria Euclidiana é uma teoria matemática que se baseia em axiomas e postulados para provar teoremas e propriedades geométricas. Com base nesse estudo, explique o que é uma congruência de triângulos.',
+    options: [
+      'Congruência de triângulos é a propriedade que afirma que dois triângulos possuem medidas iguais em todos os seus lados e ângulos',
+      'Congruência de triângulos é a propriedade que afirma que dois triângulos possuem apenas um lado e um ângulo iguais',
+      'Congruência de triângulos é a propriedade que afirma que dois triângulos possuem apenas um lado e dois ângulos iguais',
+      'Congruência de triângulos é a propriedade que afirma que dois triângulos possuem medidas diferentes em todos os seus lados e ângulos',
+    ],
+    answer:
+      'Congruência de triângulos é a propriedade que afirma que dois triângulos possuem medidas iguais em todos os seus lados e ângulos',
+  },
+  {
+    text:
+      'Os ecossistemas são formados por diversas espécies de seres vivos que interagem entre si e com o ambiente. Com base nesse estudo, explique como a poluição pode afetar o equilíbrio de um ecossistema.',
+    options: [
+      'A poluição pode aumentar a diversidade de espécies no ecossistema',
+      'A poluição pode diminuir a quantidade de nutrientes no ecossistema',
+      'A poluição pode causar a morte de algumas espécies no ecossistema',
+      'A poluição não afeta o equilíbrio de um ecossistema',
+    ],
+    answer: 'A poluição pode causar a morte de algumas espécies no ecossistema',
+  },
+  {
+    text:
+      'A genética é o estudo dos genes e hereditariedade. Com base nesse estudo, explique a diferença entre dominância completa e co-dominância em um cruzamento genético.',
+    options: [
+      'Dominância completa é quando os dois alelos são expressos igualmente, enquanto a co-dominância é quando apenas um alelo é expresso',
+      'Dominância completa é quando apenas um alelo é expresso, enquanto a co-dominância é quando os dois alelos são expressos igualmente',
+      'Dominância completa é quando apenas um alelo é expresso, enquanto a co-dominância é quando nenhum alelo é expresso',
+      'Dominância completa e co-dominância são a mesma coisa',
+    ],
+    answer:
+      'Dominância completa é quando apenas um alelo é expresso, enquanto a co-dominância é quando os dois alelos são expressos igualmente',
+  }
 ];
 
 const router = createBrowserRouter([
